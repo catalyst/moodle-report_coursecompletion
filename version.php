461e20ec438999631e2c4b0880e00027bcc8b1d7
bO Pierce <43460313+boGitsIT@users.noreply.github.com>
2019-03-08 14:12:07 +1300

    Coursecompletion (#7)
    * mdl35 /report/coursecompletion - updated plugin version number
    
    * mdl35 /report/coursecompletion - GDPR null provider class with
    legacy_polyfill

--- version.php
+++ version.php
@@ -1,7 +1,7 @@
 <?php
     defined("MOODLE_INTERNAL") || die;
 
-    $plugin->version = 2017102604;
+    $plugin->version = 2019030600;
     $plugin->requires = 2016052304.01;
     $plugin->component = "report_coursecompletion";
 ?>
