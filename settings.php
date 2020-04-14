73f354bcff3dee8274d7f942c5561aac2798310f
bO Pierce <bo.pierce@catalyst.net.nz>
2020-04-14 16:39:58 +1200

    report/coursecompletion: issue#14 Added missing defined("MOODLE_INTERNAL")
    In accordance with Issue#14,
    and https://docs.moodle.org/dev/Coding_style#Require_.2F_include
    and more simply, https://tracker.moodle.org/browse/MDL-61446
    
    WR305238 - issue#14,

--- settings.php
+++ settings.php
@@ -1,4 +1,8 @@
 <?php
+<<<<<<< HEAD
+
+    defined("MOODLE_INTERNAL") || die;
+
     $ADMIN->add("reports", new admin_externalpage("reportcoursecompletion", get_string("coursecompletion", "report_coursecompletion"), "$CFG->wwwroot/report/coursecompletion/index.php"));
     $settings = null;
 ?>
