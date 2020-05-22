ce614e67bfa2a98434ac6f04902ea97d625a1212
bO Pierce <bo.pierce@catalyst.net.nz>
2020-05-22 17:56:36 +1200

    report/coursecompletion: Remove depricated file versions
    WR305238

--- forms.php
+++ forms.php
@@ -1,4 +1,5 @@
 <?php
+
     defined("MOODLE_INTERNAL") || die;
 
     require_once __DIR__ . "/../../config.php";
@@ -6,8 +7,7 @@
 
 class ReportForm extends moodleform
 {
-    public function definition()
-    {
+    public function definition() {
         global $CFG;
         $mform = $this->_form;
 
@@ -28,30 +28,65 @@ class ReportForm extends moodleform
             $mform->addElement("advcheckbox", "deleted", get_string("form:deleted", "report_coursecompletion"));
         }
 
-        private function get_course_categories() {
-            global $DB;
-            $final_categories = [];
-            $all_categories = $DB->get_records("course_categories");
+        $mform->addElement("header", "section_coursedetails", get_string("form:section_coursedetails", "report_coursecompletion"));
+        $categories = $this->get_course_categories();
+        $mform->addElement("select", "course_categories", get_string("form:course_categories", "report_coursecompletion"), $categories);
+        $mform->setDefault("course_categories", 0);
+        $mform->addElement("text", "course", get_string("form:course", "report_coursecompletion"));
+        $complete_options = [
+            get_string("form:completed_options_any", "report_coursecompletion"),
+            get_string("form:completed_options_completed", "report_coursecompletion"),
+            get_string("form:completed_options_not_completed", "report_coursecompletion")
+        ];
+        $mform->addElement("select", "completed_options", get_string("form:completed_options", "report_coursecompletion"), $complete_options);
+        $mform->setType("course", PARAM_TEXT);
 
-            $final_categories[] = get_string("form:any_category", "report_coursecompletion");
-            foreach($all_categories as $category) {
-                $final_categories[$category->id] = $category->name;
-            }
+        $options = ['optional' => true];
+        $mform->addElement("header", "section_timecompleted", get_string("form:section_timecompleted", "report_coursecompletion"));
+        $mform->setExpanded("section_timecompleted", false);
+        $mform->addElement('date_selector', 'timecompleted_after', get_string("form:timecompleted_after", "report_coursecompletion"), $options);
+        $mform->addElement('date_selector', 'timecompleted_before', get_string("form:timecompleted_before", "report_coursecompletion"), $options);
 
-            return $final_categories;
-        }
+        $mform->addElement("header", "section_timestarted", get_string("form:section_timestarted", "report_coursecompletion"));
 
-        private function get_cohorts() {
-            global $DB;
-            $final_cohorts = [];
-            $all_cohorts = $DB->get_records("cohort");
+        $mform->addElement('date_selector', 'timestarted_after', get_string("form:timestarted_after", "report_coursecompletion"), $options);
+        $mform->addElement('date_selector', 'timestarted_before', get_string("form:timestarted_before", "report_coursecompletion"), $options);
+        $mform->closeHeaderBefore("search_operators");
+        $mform->setExpanded("section_timecompleted", false);
 
-            $final_cohorts[] = get_string("form:any_cohort", "report_coursecompletion");
-            foreach($all_cohorts as $cohort) {
-                $final_cohorts[$cohort->id] = $cohort->name;
-            }
+        $radio_array = [];
+        $radio_array[] = $mform->createElement("radio", "operator", "", get_string("form:operator_and", "report_coursecompletion"), 0);
+        $radio_array[] = $mform->createElement("static", "space", "", "<br>");
+        $radio_array[] = $mform->createElement("radio", "operator", "", get_string("form:operator_or", "report_coursecompletion"), 1);
+        $mform->addGroup($radio_array, "search_operators", get_string("form:operator", "report_coursecompletion"), array(" "), false);
+        $mform->setDefault("operator", 0);
 
-            return $final_cohorts;
-        }
+        $this->add_action_buttons(false, get_string("form:search", "report_coursecompletion"));
     }
-?>
+
+    private function get_course_categories() {
+        global $DB;
+        $final_categories = [];
+        $all_categories = $DB->get_records("course_categories");
+
+        $final_categories[] = get_string("form:any_category", "report_coursecompletion");
+        foreach ($all_categories as $category) {
+            $final_categories[$category->id] = $category->name;
+        }
+
+        return $final_categories;
+    }
+
+    private function get_cohorts() {
+        global $DB;
+        $final_cohorts = [];
+        $all_cohorts = $DB->get_records("cohort");
+
+        $final_cohorts[] = get_string("form:any_cohort", "report_coursecompletion");
+        foreach ($all_cohorts as $cohort) {
+            $final_cohorts[$cohort->id] = $cohort->name;
+        }
+
+        return $final_cohorts;
+    }
+}
--- index.php
+++ index.php
@@ -24,9 +24,9 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-require_once(__DIR__."/../../config.php");
-require_once($CFG->libdir."/adminlib.php");
-require_once($CFG->dirroot.'/report/coursecompletion/forms.php');
+require_once(__DIR__ . "/../../config.php");
+require_once($CFG->libdir . "/adminlib.php");
+require_once($CFG->dirroot . '/report/coursecompletion/forms.php');
 
 $course = $DB->get_record('course', array('id' => SITEID));
 $userid = $USER->id;
@@ -44,13 +44,13 @@ $capabilities = array('report/coursecompletion:view',
     'report/coursecompletion:view');
 $access = false;
 
-if (has_any_capability( $capabilities, $systemcontext)){ 
+if (has_any_capability( $capabilities, $systemcontext)) {
     $access = true;
 }
 
 if (!$access) {
-    // no access to coursecompletion report!
-    print_error('nopermissiontoviewcoursecompletionreport', 'error',  $CFG->wwwroot.'/my');
+    // No access to coursecompletion report!
+    print_error('nopermissiontoviewcoursecompletionreport', 'error', $CFG->wwwroot . '/my');
 }
 
 if (has_capability('report/coursecompletion:viewall', $systemcontext)) {
@@ -61,53 +61,55 @@ if (has_capability('report/coursecompletion:viewall', $systemcontext)) {
     $PAGE->set_pagelayout('report');
 }
 
-// The default column to sort by
-$default_sort = REPORT_COURSECOMPLETION_IS_ADMIN ? "u.firstname" : "c.fullname";
+// The default column to sort by.
+$defaultsort = REPORT_COURSECOMPLETION_IS_ADMIN ? "u.firstname" : "c.fullname";
 
-// Columns of the report
+// Columns of the report.
 $columns = array(
     "course",
     "timestarted",
     "timecompleted",
     "completionstatus",
 );
-// Add user columns if user is admin
+// Add user columns if user is admin.
 if (REPORT_COURSECOMPLETION_IS_ADMIN) {
     array_unshift($columns, 'name', 'email');
 }
 
-// Sort sql fields for each column
+// Sort sql fields for each column.
 $scolumns = array(
     'course' => array('c.fullname'),
     'timestarted' => array('cc.timestarted'),
     'timecompleted' => array('cc.timecompleted'),
     'completionstatus' => array('completionstatus'),
 );
-// Add user columns if user is admin
+// Add user columns if user is admin.
 if (REPORT_COURSECOMPLETION_IS_ADMIN) {
     $scolumns = array_merge(
         array(
         'name' => array('u.firstname', 'u.lastname'),
         'email' => array('u.email')
-    ), $scolumns);
+        ),
+        $scolumns
+    );
 }
 
-// Variables that hold SQL
-$user_where   = "WHERE u.id = :userid";
-$user_params  = array('userid' => $USER->id);
+// Variables that hold SQL.
+$userwhere   = "WHERE u.id = :userid";
+$userparams  = array('userid' => $USER->id);
 $where        = "";
 $params       = [];
-$cohort_join  = "";
+$cohortjoin  = "";
 
-// Build array of all the possible sort columns
+// Build array of all the possible sort columns.
 $allsorts = array();
-foreach($scolumns as $sorts) {
-    foreach($sorts as $s) {
+foreach ($scolumns as $sorts) {
+    foreach ($sorts as $s) {
         $allsorts[] = $s;
     }
 }
 
-$sort = optional_param("sort", $default_sort, PARAM_NOTAGS); // Sorting column.
+$sort = optional_param("sort", $defaultsort, PARAM_NOTAGS); // Sorting column.
 $dir = optional_param("dir", "ASC", PARAM_ALPHA);            // Sorting direction.
 $page = optional_param("page", 0, PARAM_INT);                // Page number.
 $perpage = optional_param("perpage", 30, PARAM_INT);         // Results to display per page.
@@ -119,25 +121,25 @@ $export = optional_param("export", 0, PARAM_INT);            // Export to csv th
  * values are user supplied and included in the query.
  */
 
-if(!in_array($sort, $allsorts)) {
-    $sort = $default_sort;
+if (!in_array($sort, $allsorts)) {
+    $sort = $defaultsort;
 }
 $dir = $dir === 'DESC' ? 'DESC' : 'ASC';
 
 // Ensure the maxiumum records perpage is not ever set too high.
 $perpage = min(100, $perpage);
 
-//Intialise mform.
+// Intialise mform.
 $mform = new ReportForm();
 $data = $mform->get_data();
 
-//Use the session to hold the form data in case they refresh the page after a post.
-if(!$data && isset($USER->session) && isset($USER->session['coursecompletion_formd'])) {
+// Use the session to hold the form data in case they refresh the page after a post.
+if (!$data && isset($USER->session) && isset($USER->session['coursecompletion_formd'])) {
     $data = $USER->session['coursecompletion_formd'];
     $mform->set_data($data);
 }
 
-if($data) {//Build the SQL query based on the form data.
+if ($data) {  // Build the SQL query based on the form data.
     $USER->session['coursecompletion_formd'] = $data;
     if (REPORT_COURSECOMPLETION_IS_ADMIN) {
         process_data_field($data, $where, $params, "u.firstname", "firstname", "LIKE");
@@ -150,11 +152,11 @@ if($data) {//Build the SQL query based on the form data.
     process_data_field($data, $where, $params, "c.category", "course_categories", "=");
     process_data_field($data, $where, $params, "c.fullname", "course", "LIKE");
 
-    if(isset($data->completed_options)) {
-        if($data->completed_options == 1) {
+    if (isset($data->completed_options)) {
+        if ($data->completed_options == 1) {
             add_condition_connectors($data, $where, $params);
             $where .= " cc.timecompleted IS NOT NULL";
-        } else if($data->completed_options == 2) {
+        } else if ($data->completed_options == 2) {
             add_condition_connectors($data, $where, $params);
             $where .= " cc.timecompleted IS NULL";
         }
@@ -167,7 +169,7 @@ if($data) {//Build the SQL query based on the form data.
     process_data_field($data, $where, $params, "cc.timestarted", "timestarted_before", "<=");
 
     if (REPORT_COURSECOMPLETION_IS_ADMIN && isset($data->cohorts) && $data->cohorts != 0) {
-        $cohort_join = "LEFT JOIN {cohort_members} AS cm ON u.id = cm.userid AND cm.cohortid = :cohortid";
+        $cohortjoin = "LEFT JOIN {cohort_members} cm ON u.id = cm.userid AND cm.cohortid = :cohortid";
         add_condition_connectors($data, $where, $params);
         $where .= " cm.id IS NOT NULL ";
         $params["cohortid"] = $data->cohorts;
@@ -178,66 +180,65 @@ if($data) {//Build the SQL query based on the form data.
 // admin, so that only that user's records show.
 if (!REPORT_COURSECOMPLETION_IS_ADMIN) {
     if ($where == "") {
-        $where = $user_where;
+        $where = $userwhere;
     } else {
-        $where = "$user_where AND ($where)";
+        $where = "$userwhere AND ($where)";
     }
 }
 if (!REPORT_COURSECOMPLETION_IS_ADMIN) {
-    $params = array_merge($user_params, $params);
+    $params = array_merge($userparams, $params);
 }
-if (!IS_ADMIN) $params = array_merge($user_params, $params);
 
 if (REPORT_COURSECOMPLETION_IS_ADMIN && $where != "") {
     $where = "WHERE $where";
 }
 
-$order_by = "ORDER BY $sort $dir";
+$orderby = "ORDER BY $sort $dir";
 
 // Get list of user fields for display.
-$user_cols = REPORT_COURSECOMPLETION_IS_ADMIN ? get_all_user_name_fields(true, 'u') . ", u.email, " : "";
+$usercols = REPORT_COURSECOMPLETION_IS_ADMIN ? get_all_user_name_fields(true, 'u') . ", u.email, " : "";
 
-//Generate the final SQL.
-$sql = "SELECT cc.id, cc.userid,$user_cols cc.course, c.fullname, cc.timestarted, cc.timecompleted
+// Generate the final SQL.
+$sql = "SELECT cc.id, cc.userid,$usercols cc.course, c.fullname, cc.timestarted, cc.timecompleted
         FROM {course_completions} AS cc
         JOIN {user} AS u ON cc.userid = u.id
         JOIN {course} AS c ON cc.course = c.id
-        $cohort_join $where $order_by";
+        $cohrtjoin $where $orderby";
 
-//The sql for the parameterised count.
-$count_sql = "SELECT COUNT(cc.id)
+// The sql for the parameterised count.
+$countsql = "SELECT COUNT(cc.id)
         FROM {course_completions} AS cc
         JOIN {user} AS u ON cc.userid = u.id
         JOIN {course} AS c ON cc.course = c.id
-        $cohort_join $where";
+        $cohortjoin $where";
 
-//The sql for the total count.
-$count_sql_total = "SELECT COUNT(cc.id)
+// The sql for the total count.
+$countsqltotal = "SELECT COUNT(cc.id)
         FROM {course_completions} AS cc
         JOIN {user} AS u ON cc.userid = u.id
         JOIN {course} AS c ON cc.course = c.id
-        $user_where";
+        $userwhere";
 
-//If requested, dump to csv instead, use a recordset because the size could grow large.
-if($export) {
+// If requested, dump to csv instead, use a recordset because the size could grow large.
+if ($export) {
     $records = $DB->get_recordset_sql($sql, $params);
-    // output headers so that the file is downloaded rather than displayed.
+    // Output headers so that the file is downloaded rather than displayed.
     header('Content-Type: text/csv; charset=utf-8');
     header('Content-Disposition: attachment; filename=data.csv');
 
-    // create a file pointer connected to the output stream.
+    // Create a file pointer connected to the output stream.
     $output = fopen('php://output', 'w');
 
-    //Generate column headers.
+    // Generate column headers.
     $columnsh = array();
-    foreach($columns as $column) {
+    foreach ($columns as $column) {
         $columnsh[] = get_string("table:export_header_$column", 'report_coursecompletion');
     }
 
-    //Output the column headings.
+    // Output the column headings.
     fputcsv($output, $columnsh);
 
-    //Now dump the data.
+    // Now dump the data.
     $final = new stdClass();
     foreach ($records as $record) {
         if (REPORT_COURSECOMPLETION_IS_ADMIN) {
@@ -251,66 +252,71 @@ if($export) {
         fputcsv($output, (array)$final);
     }
 
-    //Close the recordset.
+    // Close the recordset.
     $records->close();
 
-    //We're done.
+    // We're done.
     die;
 }
 
-//Get records and required count values.
-$currentstart = $page * $perpage; //Count of where to start with records
+// Get records and required count values.
+$currentstart = $page * $perpage; // Count of where to start with records.
 $records = $DB->get_records_sql($sql, $params, $currentstart, $perpage);
 
-$total_record_count = REPORT_COURSECOMPLETION_IS_ADMIN ? $DB->count_records("course_completions") : $DB->count_records_sql($count_sql_total, $user_params);
-$changes_count = $DB->count_records_sql($count_sql, $params);
+$totalrecordcount = REPORT_COURSECOMPLETION_IS_ADMIN
+    ? $DB->count_records("course_completions")
+    : $DB->count_records_sql($countsqltotal, $userparams);
+$changescount = $DB->count_records_sql($countsql, $params);
 
-//Start display of the page, itself.
+// Start display of the page, itself.
 
 echo $OUTPUT->header();
-$header_string = REPORT_COURSECOMPLETION_IS_ADMIN ? get_string("report_header_admin", "report_coursecompletion") : get_string("report_header", "report_coursecompletion") . fullname($USER, true);
-echo $OUTPUT->heading($header_string);
+$headerstring = REPORT_COURSECOMPLETION_IS_ADMIN
+    ? get_string("report_header_admin", "report_coursecompletion")
+    : get_string("report_header", "report_coursecompletion")
+    . fullname($USER, true);
+echo $OUTPUT->heading($headerstring);
 
 $mform->display();
 
-//Url used for sorting the table.
-$base_url = new moodle_url("index.php", array("sort" => $sort, "dir" => $dir, "perpage" => $perpage, "page" => $page));
+// Url used for sorting the table.
+$baseurl = new moodle_url("index.php", array("sort" => $sort, "dir" => $dir, "perpage" => $perpage, "page" => $page));
 
-//Count of records shown as per filter options + href anchor link for jumping back to the table after a search or sort.
-$a = new StdClass;
-$a->total = $total_record_count;
-$a->filter = $changes_count;
+// Count of records shown as per filter options + href anchor link for jumping back to the table after a search or sort.
+$a = new StdClass();
+$a->total = $totalrecordcount;
+$a->filter = $changescount;
 echo get_string("count_string", "report_coursecompletion", $a);
 echo '<a name="table"></a>';
 
-echo $OUTPUT->paging_bar($changes_count, $page, $perpage, $base_url);
+echo $OUTPUT->paging_bar($changescount, $page, $perpage, $baseurl);
 
-//Calculate table headers (clickable links that do sorting).
+// Calculate table headers (clickable links that do sorting).
 $hcolumns = array();
-//Foreach column we look at it's applicable sort columns and build a final link header.
-foreach($columns as $column) {
+// Foreach column we look at it's applicable sort columns and build a final link header.
+foreach ($columns as $column) {
     $final = array();
-    foreach($scolumns[$column] as $sortcolumn) {
-        if($sort != $sortcolumn) {
-            $column_dir = $dir;
-            $column_icon = "";
+    foreach ($scolumns[$column] as $sortcolumn) {
+        if ($sort != $sortcolumn) {
+            $columndir = $dir;
+            $columnicon = "";
         } else {
-            $column_dir = $dir == "ASC" ? "DESC" : "ASC";
-            $column_icon_dir = ($dir == "ASC") ? "down" : "up";
-            $column_icon = " <img src=\"" . $OUTPUT->pix_url("t/" . $column_icon_dir) . "\" alt=\"\" />";
+            $columndir = $dir == "ASC" ? "DESC" : "ASC";
+            $columnicondir = ($dir == "ASC") ? "down" : "up";
+            $columnicon = " <img src=\"" . $OUTPUT->pix_url("t/" . $columnicondir) . "\" alt=\"\" />";
         }
-        //Get a string for this sort link.
-        $column_header = get_string("table:sort_header_$sortcolumn", 'report_coursecompletion');
-        //Update parameters for sort and direction for this column in the final url.
-        $base_url->param('sort', $sortcolumn);
-        $base_url->param('dir', $column_dir);
-        if ( $sortcolumn === "completionstatus") {
-            $final[] = "<a href=javascript:void(0)>$column_header</a>";
+        // Get a string for this sort link.
+        $columnheader = get_string("table:sort_header_$sortcolumn", 'report_coursecompletion');
+        // Update parameters for sort and direction for this column in the final url.
+        $baseurl->param('sort', $sortcolumn);
+        $baseurl->param('dir', $columndir);
+        if ($sortcolumn === "completionstatus") {
+            $final[] = "<a href=javascript:void(0)>$columnheader</a>";
         } else {
-            $final[] = "<a href=$base_url#table>$column_header</a>$column_icon";
+            $final[] = "<a href=$baseurl#table>$columnheader</a>$columnicon";
         }
     }
-    //If one column has multiple sorts, combine them into one entry for that column.
+    // If one column has multiple sorts, combine them into one entry for that column.
     $hcolumns[$column] = implode('/', $final);
 }
 
@@ -318,14 +324,14 @@ $table = new html_table();
 $table->head = $hcolumns;
 $table->attributes["class"] = "admintable generaltable";
 $table->data = [];
-foreach($records as $record) {
+foreach ($records as $record) {
     $final = new stdClass();
     if (REPORT_COURSECOMPLETION_IS_ADMIN) {
         $finalname = fullname($record);
-        $final->fullname = add_link('/user/view.php', array('id' =>$record->userid), $finalname);
+        $final->fullname = add_link('/user/view.php', array('id' => $record->userid), $finalname);
         $final->email = $record->email;
     }
-    $final->course = add_link('/course/view.php', array('id' =>$record->course), $record->fullname);
+    $final->course = add_link('/course/view.php', array('id' => $record->course), $record->fullname);
     $final->timestarted = time_format($record->timestarted);
     $final->timecompleted = time_format($record->timecompleted);
     $final->completionstatus = $record->timecompleted ? 'Yes' : 'No';
@@ -333,14 +339,22 @@ foreach($records as $record) {
 }
 
 echo html_writer::table($table);
-$buttonurl = new moodle_url("index.php", array("sort" => $sort, "dir" => $dir, "perpage" => $perpage, "page" => $page, "export"=>1));
+$buttonurl = new moodle_url("index.php", array(
+    "sort" => $sort,
+    "dir" => $dir,
+    "perpage" => $perpage,
+    "page" => $page,
+    "export" => 1
+    )
+);
 $buttonstring = get_string('exportbutton', 'report_coursecompletion');
 echo $OUTPUT->single_button($buttonurl, $buttonstring);
 echo $OUTPUT->footer();
 
-function add_condition_connectors(&$data, &$where, &$params, $force_and = false) {
-    if(!empty($params)) {
-        if($force_and || (isset($data->operator) && $data->operator == 0)) {
+function add_condition_connectors(
+    &$data, &$where, &$params, $forceand = false) {
+    if (!empty($params)) {
+        if ($forceand || (isset($data->operator) && $data->operator == 0)) {
             $where .= " AND ";
         } else {
             $where .= " OR ";
@@ -352,37 +366,38 @@ function add_link($url, $params, $string) {
     return html_writer::link(new moodle_url($url, $params), $string);
 }
 
-function process_data_field(&$data, &$where, &$params, $db_field, $field_name, $comparison, $start_group = false, $force_and = false, $end_group = false) {
-    if(isset($data->{"$field_name"}) && $data->{"$field_name"}) {
-        add_condition_connectors($data, $where, $params, $force_and);
-        $params[$field_name] = $data->{"$field_name"}.($comparison == "LIKE" ? "%" : "");
+function process_data_field(&$data, &$where, &$params, $dbfield, $fieldname,
+    $comparison, $startgroup = false, $forceand = false, $endgroup = false) {
 
-        if($start_group) {
+    if (isset($data->{"$fieldname"}) && $data->{"$fieldname"}) {
+        add_condition_connectors($data, $where, $params, $forceand);
+        $params[$fieldname] = $data->{"$fieldname"} . ($comparison == "LIKE" ? "%" : "");
+
+        if ($startgroup) {
             $where .= "(";
         }
 
         $colon = ":";
 
-        // If the search parameter is a string, make it case insensitive
-        if(preg_match("/[a-z]/i", $params[$field_name])) {
-            $db_field = "LOWER($db_field)";
-            $field_name = "LOWER($colon$field_name)";
+        // If the search parameter is a string, make it case insensitive.
+        if (preg_match("/[a-z]/i", $params[$fieldname])) {
+            $dbfield = "LOWER($dbfield)";
+            $fieldname = "LOWER($colon$field_name)";
             $colon = "";
         }
 
-        $where .= "$db_field $comparison $colon$field_name";
+        $where .= "$dbfield $comparison $colon$field_name";
 
-        if($end_group) {
+        if ($endgroup) {
             $where .= ")";
         }
     }
 }
 
 function time_format($time) {
-    if(isset($time) && $time != 0) {
+    if (isset($time) && $time != 0) {
         return userdate($time);
     } else {
         return "-";
     }
 }
-?>
