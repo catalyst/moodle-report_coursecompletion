f3df778c5395739ccb41bf206977f68a6d593a7e
bO Pierce <bo.pierce@catalyst.net.nz>
2020-06-24 17:15:39 +1200

    report/coursecompletion/lang/en/report_coursecompletion: Fix style errors
    WR305238 - standard: moodle (Moodle Code Checker)

--- lang/en/report_coursecompletion.php
+++ lang/en/report_coursecompletion.php
@@ -1,4 +1,29 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Coursecompletion report
+ *
+ * @package    report
+ * @subpackage coursecompletion
+ * @copyright  2017 Catalyst IT Ltd
+ * @author     Francis Devine <francis@catalyst.net.nz>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
     $string['pluginname'] = 'Course completion';
     $string['coursecompletion'] = 'Course completion';
     $string['report_header_admin'] = 'Course completion records for all users';
