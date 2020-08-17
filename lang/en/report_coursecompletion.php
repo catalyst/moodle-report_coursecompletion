33739af01209bdfa4b8b84562492a2b22f5430da
Dan Marsden <dan@danmarsden.com>
2020-08-17 14:35:26 +1200

    Sort lang strings correctly.

--- lang/en/report_coursecompletion.php
+++ lang/en/report_coursecompletion.php
@@ -22,57 +22,53 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$string['pluginname'] = 'Course completion';
-$string['coursecompletion'] = 'Course completion';
-$string['report_header_admin'] = 'Course completion records for all users';
-$string['report_header'] = 'Course completion record for ';
+// Privacy API.
 $string['count_string'] = '{$a->filter} shown of total: {$a->total}';
+$string['coursecompletion'] = 'Course completion';
 $string['exportbutton'] = 'Export csv';
-
-$string['table:export_header_name'] = 'Fullname';
-$string['table:export_header_email'] = 'Email';
-$string['table:export_header_course'] = 'Course';
-$string['table:export_header_timestarted'] = 'Time started';
-$string['table:export_header_timecompleted'] = 'Time completed';
-$string['table:export_header_completionstatus']  = 'Complete';
-
-$string['table:sort_header_u.firstname']  = 'Firstname';
-$string['table:sort_header_u.lastname']  = 'Lastname';
-$string['table:sort_header_u.email']  = 'Email';
-$string['table:sort_header_c.fullname']  = 'Course';
-$string['table:sort_header_cc.timecompleted']  = 'Timecompleted';
-$string['table:sort_header_cc.timestarted']  = 'Timestarted';
-$string['table:sort_header_completionstatus']  = 'Complete';
-
 $string['form:active'] = 'Show active';
-$string['form:suspended'] = 'Hide suspended';
-$string['form:deleted'] = 'Hide deleted';
-$string['form:cohorts'] = 'Cohort:';
+$string['form:any_category'] = 'Any';
 $string['form:any_cohort'] = 'Any';
-$string['form:section_userdetails'] = 'User details';
-$string['form:firstname'] = 'First name:';
-$string['form:lastname'] = 'Last name:';
-$string['form:email'] = 'Email:';
+$string['form:cohorts'] = 'Cohort:';
 $string['form:completed_options'] = 'Course completion';
 $string['form:completed_options_any'] = 'Any';
 $string['form:completed_options_completed'] = 'Completed';
 $string['form:completed_options_not_completed'] = 'Not completed';
-$string['form:search'] = 'Search';
-$string['form:section_coursedetails'] = 'Course details';
-$string['form:course_categories'] = 'Category:';
-$string['form:any_category'] = 'Any';
 $string['form:course'] = 'Course:';
-$string['form:section_timecompleted'] = 'Time completed';
+$string['form:course_categories'] = 'Category:';
+$string['form:deleted'] = 'Hide deleted';
+$string['form:email'] = 'Email:';
 $string['form:filter_by_timecompleted'] = 'Filter by time completed:';
-$string['form:timecompleted_after'] = 'Completed after:';
-$string['form:timecompleted_before'] = 'Completed before:';
-$string['form:section_timestarted'] = 'Time started';
 $string['form:filter_by_timestarted'] = 'Filter by time started:';
-$string['form:timestarted_after'] = 'Started after:';
-$string['form:timestarted_before'] = 'Started before:';
+$string['form:firstname'] = 'First name:';
+$string['form:lastname'] = 'Last name:';
+$string['form:operator'] = 'Show records that match:';
 $string['form:operator_and'] = 'All conditions';
 $string['form:operator_or'] = 'Any condition';
-$string['form:operator'] = 'Show records that match:';
-
-// Privacy API.
+$string['form:search'] = 'Search';
+$string['form:section_coursedetails'] = 'Course details';
+$string['form:section_timecompleted'] = 'Time completed';
+$string['form:section_timestarted'] = 'Time started';
+$string['form:section_userdetails'] = 'User details';
+$string['form:suspended'] = 'Hide suspended';
+$string['form:timecompleted_after'] = 'Completed after:';
+$string['form:timecompleted_before'] = 'Completed before:';
+$string['form:timestarted_after'] = 'Started after:';
+$string['form:timestarted_before'] = 'Started before:';
+$string['pluginname'] = 'Course completion';
 $string['privacy:no_userid_data'] = 'The course completion plugin does not share data to external services, store user preferences, or identify users in its database.';
+$string['report_header'] = 'Course completion record for ';
+$string['report_header_admin'] = 'Course completion records for all users';
+$string['table:export_header_completionstatus']  = 'Complete';
+$string['table:export_header_course'] = 'Course';
+$string['table:export_header_email'] = 'Email';
+$string['table:export_header_name'] = 'Fullname';
+$string['table:export_header_timecompleted'] = 'Time completed';
+$string['table:export_header_timestarted'] = 'Time started';
+$string['table:sort_header_c.fullname']  = 'Course';
+$string['table:sort_header_cc.timecompleted']  = 'Timecompleted';
+$string['table:sort_header_cc.timestarted']  = 'Timestarted';
+$string['table:sort_header_completionstatus']  = 'Complete';
+$string['table:sort_header_u.email']  = 'Email';
+$string['table:sort_header_u.firstname']  = 'Firstname';
+$string['table:sort_header_u.lastname']  = 'Lastname';
