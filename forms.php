37aacdfd09f4c0e079c686bee13e7c551390cc69
Dan Marsden <dan@danmarsden.com>
2020-08-17 14:23:02 +1200

    Fix #10 - fix boilerplates.

--- forms.php
+++ forms.php
@@ -1,91 +1,113 @@
 <?php
-    defined("MOODLE_INTERNAL") || die;
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-    require_once(__DIR__."/../../config.php");
-    require_once($CFG->libdir."/formslib.php");
+/**
+ * Coursecompletion reportform class.
+ *
+ * @package    report_coursecompletion
+ * @copyright  2017 Catalyst IT Ltd
+ * @author     Oliver Redding <oliverredding@catalyst.net.nz>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 
-    class ReportForm extends moodleform {
-        public function definition() {
-            global $CFG;
-            $mform = $this->_form;
+defined("MOODLE_INTERNAL") || die;
 
-            if (IS_ADMIN) {
-                $mform->addElement("header", "section_userdetails", get_string("form:section_userdetails", "report_coursecompletion"));
-                $mform->addElement("text", "firstname", get_string("form:firstname", "report_coursecompletion"));
-                $mform->setType("firstname", PARAM_ALPHA);
-                $mform->addElement("text", "lastname", get_string("form:lastname", "report_coursecompletion"));
-                $mform->setType("lastname", PARAM_ALPHA);
-                $mform->addElement("text", "email", get_string("form:email", "report_coursecompletion"));
-                $mform->setType("email", PARAM_NOTAGS);
-                $cohorts = $this->get_cohorts();
-                $mform->addElement("select", "cohorts", get_string("form:cohorts", "report_coursecompletion"), $cohorts);
-                $mform->setDefault("cohorts", 0);
+require_once($CFG->libdir."/formslib.php");
 
-                $mform->addElement("advcheckbox", "active", get_string("form:active", "report_coursecompletion"));
-                $mform->addElement("advcheckbox", "suspended", get_string("form:suspended", "report_coursecompletion"));
-                $mform->addElement("advcheckbox", "deleted", get_string("form:deleted", "report_coursecompletion"));
-            }
+class ReportForm extends moodleform {
+    public function definition() {
+        $mform = $this->_form;
 
-            $mform->addElement("header", "section_coursedetails", get_string("form:section_coursedetails", "report_coursecompletion"));
-            $categories = $this->get_course_categories();
-            $mform->addElement("select", "course_categories", get_string("form:course_categories", "report_coursecompletion"), $categories);
-            $mform->setDefault("course_categories", 0);
-            $mform->addElement("text", "course", get_string("form:course", "report_coursecompletion"));
-            $complete_options = [
-                get_string("form:completed_options_any", "report_coursecompletion"),
-                get_string("form:completed_options_completed", "report_coursecompletion"),
-                get_string("form:completed_options_not_completed", "report_coursecompletion")
-            ];
-            $mform->addElement("select", "completed_options", get_string("form:completed_options", "report_coursecompletion"), $complete_options);
-            $mform->setType("course", PARAM_TEXT);
+        if (IS_ADMIN) {
+            $mform->addElement("header", "section_userdetails", get_string("form:section_userdetails", "report_coursecompletion"));
+            $mform->addElement("text", "firstname", get_string("form:firstname", "report_coursecompletion"));
+            $mform->setType("firstname", PARAM_ALPHA);
+            $mform->addElement("text", "lastname", get_string("form:lastname", "report_coursecompletion"));
+            $mform->setType("lastname", PARAM_ALPHA);
+            $mform->addElement("text", "email", get_string("form:email", "report_coursecompletion"));
+            $mform->setType("email", PARAM_NOTAGS);
+            $cohorts = $this->get_cohorts();
+            $mform->addElement("select", "cohorts", get_string("form:cohorts", "report_coursecompletion"), $cohorts);
+            $mform->setDefault("cohorts", 0);
 
-            $options = ['optional' => true];
-            $mform->addElement("header", "section_timecompleted", get_string("form:section_timecompleted", "report_coursecompletion"));
-            $mform->setExpanded("section_timecompleted", false);
-            $mform->addElement('date_selector', 'timecompleted_after', get_string("form:timecompleted_after", "report_coursecompletion"), $options);
-            $mform->addElement('date_selector', 'timecompleted_before', get_string("form:timecompleted_before", "report_coursecompletion"), $options);
-
-            $mform->addElement("header", "section_timestarted", get_string("form:section_timestarted", "report_coursecompletion"));
-
-            $mform->addElement('date_selector', 'timestarted_after', get_string("form:timestarted_after", "report_coursecompletion"), $options);
-            $mform->addElement('date_selector', 'timestarted_before', get_string("form:timestarted_before", "report_coursecompletion"), $options);
-            $mform->closeHeaderBefore("search_operators");
-            $mform->setExpanded("section_timecompleted", false);
-
-            $radio_array = [];
-            $radio_array[] = $mform->createElement("radio", "operator", "", get_string("form:operator_and", "report_coursecompletion"), 0);
-            $radio_array[] = $mform->createElement("static", "space", "", "<br>");
-            $radio_array[] = $mform->createElement("radio", "operator", "", get_string("form:operator_or", "report_coursecompletion"), 1);
-            $mform->addGroup($radio_array, "search_operators", get_string("form:operator", "report_coursecompletion"), array(" "), false);
-            $mform->setDefault("operator", 0);
-
-            $this->add_action_buttons(false, get_string("form:search", "report_coursecompletion"));
+            $mform->addElement("advcheckbox", "active", get_string("form:active", "report_coursecompletion"));
+            $mform->addElement("advcheckbox", "suspended", get_string("form:suspended", "report_coursecompletion"));
+            $mform->addElement("advcheckbox", "deleted", get_string("form:deleted", "report_coursecompletion"));
         }
 
-        private function get_course_categories() {
-            global $DB;
-            $final_categories = [];
-            $all_categories = $DB->get_records("course_categories");
+        $mform->addElement("header", "section_coursedetails", get_string("form:section_coursedetails", "report_coursecompletion"));
+        $categories = $this->get_course_categories();
+        $mform->addElement("select", "course_categories", get_string("form:course_categories", "report_coursecompletion"), $categories);
+        $mform->setDefault("course_categories", 0);
+        $mform->addElement("text", "course", get_string("form:course", "report_coursecompletion"));
+        $complete_options = [
+            get_string("form:completed_options_any", "report_coursecompletion"),
+            get_string("form:completed_options_completed", "report_coursecompletion"),
+            get_string("form:completed_options_not_completed", "report_coursecompletion")
+        ];
+        $mform->addElement("select", "completed_options", get_string("form:completed_options", "report_coursecompletion"), $complete_options);
+        $mform->setType("course", PARAM_TEXT);
 
-            $final_categories[] = get_string("form:any_category", "report_coursecompletion");
-            foreach($all_categories as $category) {
-                $final_categories[$category->id] = $category->name;
-            }
+        $options = ['optional' => true];
+        $mform->addElement("header", "section_timecompleted", get_string("form:section_timecompleted", "report_coursecompletion"));
+        $mform->setExpanded("section_timecompleted", false);
+        $mform->addElement('date_selector', 'timecompleted_after', get_string("form:timecompleted_after", "report_coursecompletion"), $options);
+        $mform->addElement('date_selector', 'timecompleted_before', get_string("form:timecompleted_before", "report_coursecompletion"), $options);
 
-            return $final_categories;
-        }
+        $mform->addElement("header", "section_timestarted", get_string("form:section_timestarted", "report_coursecompletion"));
 
-        private function get_cohorts() {
-            global $DB;
-            $final_cohorts = [];
-            $all_cohorts = $DB->get_records("cohort");
+        $mform->addElement('date_selector', 'timestarted_after', get_string("form:timestarted_after", "report_coursecompletion"), $options);
+        $mform->addElement('date_selector', 'timestarted_before', get_string("form:timestarted_before", "report_coursecompletion"), $options);
+        $mform->closeHeaderBefore("search_operators");
+        $mform->setExpanded("section_timecompleted", false);
 
-            $final_cohorts[] = get_string("form:any_cohort", "report_coursecompletion");
-            foreach($all_cohorts as $cohort) {
-                $final_cohorts[$cohort->id] = $cohort->name;
-            }
+        $radio_array = [];
+        $radio_array[] = $mform->createElement("radio", "operator", "", get_string("form:operator_and", "report_coursecompletion"), 0);
+        $radio_array[] = $mform->createElement("static", "space", "", "<br>");
+        $radio_array[] = $mform->createElement("radio", "operator", "", get_string("form:operator_or", "report_coursecompletion"), 1);
+        $mform->addGroup($radio_array, "search_operators", get_string("form:operator", "report_coursecompletion"), array(" "), false);
+        $mform->setDefault("operator", 0);
 
-            return $final_cohorts;
-        }
+        $this->add_action_buttons(false, get_string("form:search", "report_coursecompletion"));
     }
+
+    private function get_course_categories() {
+        global $DB;
+        $final_categories = [];
+        $all_categories = $DB->get_records("course_categories");
+
+        $final_categories[] = get_string("form:any_category", "report_coursecompletion");
+        foreach ($all_categories as $category) {
+            $final_categories[$category->id] = $category->name;
+        }
+
+        return $final_categories;
+    }
+
+    private function get_cohorts() {
+        global $DB;
+        $final_cohorts = [];
+        $all_cohorts = $DB->get_records("cohort");
+
+        $final_cohorts[] = get_string("form:any_cohort", "report_coursecompletion");
+        foreach ($all_cohorts as $cohort) {
+            $final_cohorts[$cohort->id] = $cohort->name;
+        }
+
+        return $final_cohorts;
+    }
+}
 
--- lang/en/report_coursecompletion.php
+++ lang/en/report_coursecompletion.php
@@ -1,55 +1,78 @@
 <?php
-    $string['pluginname'] = 'Course Completion';
-    $string['coursecompletion'] = 'Course Completion';
-    $string['report_header_admin'] = 'Course Completion Records for All Users';
-    $string['report_header'] = 'Course Completion Record for ';
-    $string['count_string'] = '{$a->filter} shown of total: {$a->total}';
-    $string['exportbutton'] = 'Export csv';
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-    $string['table:export_header_name'] = 'Fullname';
-    $string['table:export_header_email'] = 'Email';
-    $string['table:export_header_course'] = 'Course';
-    $string['table:export_header_timestarted'] = 'Time Started';
-    $string['table:export_header_timecompleted'] = 'Time Completed';
-    $string['table:export_header_completionstatus']  = 'Complete';
+/**
+ * Strings for component 'report_coursecompletion'
+ *
+ * @package   report_coursecompletion
+ * @copyright 2020 Catalyst IT
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 
-    $string['table:sort_header_u.firstname']  = 'Firstname';
-    $string['table:sort_header_u.lastname']  = 'Lastname';
-    $string['table:sort_header_u.email']  = 'Email';
-    $string['table:sort_header_c.fullname']  = 'Course';
-    $string['table:sort_header_cc.timecompleted']  = 'Timecompleted';
-    $string['table:sort_header_cc.timestarted']  = 'Timestarted';
-    $string['table:sort_header_completionstatus']  = 'Complete';
+$string['pluginname'] = 'Course Completion';
+$string['coursecompletion'] = 'Course Completion';
+$string['report_header_admin'] = 'Course Completion Records for All Users';
+$string['report_header'] = 'Course Completion Record for ';
+$string['count_string'] = '{$a->filter} shown of total: {$a->total}';
+$string['exportbutton'] = 'Export csv';
 
-    $string['form:active'] = 'Show active';
-    $string['form:suspended'] = 'Hide suspended';
-    $string['form:deleted'] = 'Hide deleted';
-    $string['form:cohorts'] = 'Cohort:';
-    $string['form:any_cohort'] = 'Any';
-    $string['form:section_userdetails'] = 'User details';
-    $string['form:firstname'] = 'First name:';
-    $string['form:lastname'] = 'Last name:';
-    $string['form:email'] = 'Email:';
-    $string['form:completed_options'] = 'Course completion';
-    $string['form:completed_options_any'] = 'Any';
-    $string['form:completed_options_completed'] = 'Completed';
-    $string['form:completed_options_not_completed'] = 'Not completed';
-    $string['form:search'] = 'Search';
-    $string['form:section_coursedetails'] = 'Course details';
-    $string['form:course_categories'] = 'Category:';
-    $string['form:any_category'] = 'Any';
-    $string['form:course'] = 'Course:';
-    $string['form:section_timecompleted'] = 'Time completed';
-    $string['form:filter_by_timecompleted'] = 'Filter by time completed:';
-    $string['form:timecompleted_after'] = 'Completed after:';
-    $string['form:timecompleted_before'] = 'Completed before:';
-    $string['form:section_timestarted'] = 'Time started';
-    $string['form:filter_by_timestarted'] = 'Filter by time started:';
-    $string['form:timestarted_after'] = 'Started after:';
-    $string['form:timestarted_before'] = 'Started before:';
-    $string['form:operator_and'] = 'All conditions';
-    $string['form:operator_or'] = 'Any condition';
-    $string['form:operator'] = 'Show records that match:';
+$string['table:export_header_name'] = 'Fullname';
+$string['table:export_header_email'] = 'Email';
+$string['table:export_header_course'] = 'Course';
+$string['table:export_header_timestarted'] = 'Time Started';
+$string['table:export_header_timecompleted'] = 'Time Completed';
+$string['table:export_header_completionstatus']  = 'Complete';
 
-   // Privacy API.
-   $string['privacy:no_userid_data'] = 'The course completion plugin does not share data to external services, store user preferences, or identify users in its database.';
+$string['table:sort_header_u.firstname']  = 'Firstname';
+$string['table:sort_header_u.lastname']  = 'Lastname';
+$string['table:sort_header_u.email']  = 'Email';
+$string['table:sort_header_c.fullname']  = 'Course';
+$string['table:sort_header_cc.timecompleted']  = 'Timecompleted';
+$string['table:sort_header_cc.timestarted']  = 'Timestarted';
+$string['table:sort_header_completionstatus']  = 'Complete';
+
+$string['form:active'] = 'Show active';
+$string['form:suspended'] = 'Hide suspended';
+$string['form:deleted'] = 'Hide deleted';
+$string['form:cohorts'] = 'Cohort:';
+$string['form:any_cohort'] = 'Any';
+$string['form:section_userdetails'] = 'User details';
+$string['form:firstname'] = 'First name:';
+$string['form:lastname'] = 'Last name:';
+$string['form:email'] = 'Email:';
+$string['form:completed_options'] = 'Course completion';
+$string['form:completed_options_any'] = 'Any';
+$string['form:completed_options_completed'] = 'Completed';
+$string['form:completed_options_not_completed'] = 'Not completed';
+$string['form:search'] = 'Search';
+$string['form:section_coursedetails'] = 'Course details';
+$string['form:course_categories'] = 'Category:';
+$string['form:any_category'] = 'Any';
+$string['form:course'] = 'Course:';
+$string['form:section_timecompleted'] = 'Time completed';
+$string['form:filter_by_timecompleted'] = 'Filter by time completed:';
+$string['form:timecompleted_after'] = 'Completed after:';
+$string['form:timecompleted_before'] = 'Completed before:';
+$string['form:section_timestarted'] = 'Time started';
+$string['form:filter_by_timestarted'] = 'Filter by time started:';
+$string['form:timestarted_after'] = 'Started after:';
+$string['form:timestarted_before'] = 'Started before:';
+$string['form:operator_and'] = 'All conditions';
+$string['form:operator_or'] = 'Any condition';
+$string['form:operator'] = 'Show records that match:';
+
+// Privacy API.
+$string['privacy:no_userid_data'] = 'The course completion plugin does not share data to external services, store user preferences, or identify users in its database.';
